f125e7eb2ccab062bf1cf1b5212315c49d9e0abf
Taylor Williams <taylor@thewilliams.net>
2019-03-04 16:53:48 +0000

    Automatically backed up by Learn

--- db/migrate/20190304165225_add_description_inventory_to_products.rb
+++ db/migrate/20190304165225_add_description_inventory_to_products.rb
@@ -1,4 +1,6 @@
 class AddDescriptionInventoryToProducts < ActiveRecord::Migration
   def change
+    add_column :products, :inventory, :string
+    add_column :products, :description, :string
   end
 end
