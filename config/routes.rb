a2842669e31db4ed562400ad96bfba4d6a380eb1
Taylor Williams <taylor@thewilliams.net>
2019-03-04 17:09:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:create, :index, new]
+  resources :products, only: [:create, :index, :new]
   get '/product/:id/inventory', to: 'products#inventory'
   get '/product/:id/description', to: 'products#description'
 end
