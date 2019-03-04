7b5e22197b18849000cf7579eda7756a916e7ed5
Taylor Williams <taylor@thewilliams.net>
2019-03-04 17:09:45 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
 
+  def new
+    @product = Product.new
+  end
+
   def create
     @product = Product.create(product_params)
     redirect_to products_path
