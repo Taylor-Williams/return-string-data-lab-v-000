7756ff4c7fbf561f930e74ae7fc1ea9a2e246cad
Taylor Williams <taylor@thewilliams.net>
2019-03-04 16:47:48 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
 
   def create
-
+    @product = Product.create(product_params)
   end
+
+  private
+  
+    def prodcut_params
+      params.require(:product).permit(:name, :price, :inventory, :description)
+    end
 end
