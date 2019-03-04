dfeb651ef0d6b6397ce88db7159a4203ad40d3eb
Taylor Williams <taylor@thewilliams.net>
2019-03-04 16:49:09 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,6 @@ class ProductsController < ApplicationController
   private
 
     def product_params
-      params.require(:product).permit(:name, :price, :description)
+      params.require(:product).permit(:name, :price)
     end
 end
